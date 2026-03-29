c0e580ccc6102658d08916d701f16dd58e6cc7b0
Karthik <your-email@gmail.com>
2026-03-29 16:27:22 +0530

    Update API URL for production

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API = "http://127.0.0.1:8000/api/v1";
+const API = "https://rbac-task-api-fastapi.onrender.com/api/v1";
 
 // ================= REGISTER =================
 async function register() {
